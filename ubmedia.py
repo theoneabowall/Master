a7379cec4471a800d6a5a9f6abc69d81c6fea46e
alicenewbot <81892911+paavampayyan@users.noreply.github.com>
2021-06-06 08:11:00 +0530

    ok

--- ubmedia.py
+++ ubmedia.py
@@ -1,14 +1,26 @@
 from pyrogram import Client
 from pyrogram import filters
 from apscheduler.schedulers.background import BackgroundScheduler
+import os
+
+from pyrogram.types.messages_and_media import message
+
+#---------------------------------+ heroku
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+string = os.environ["SESSION_STRING"]
+group = list(int(os.environ["GROUP_IDS"]).split())
+channel = int(os.environ["SESSION_STRING"])
+g_time = int(os.environ["SESSION_STRING"])
+c_time = int(os.environ["SESSION_STRING"])
+
 
 #---------------------------------+ Data
-api_id=5282591
-api_hash='d416fe4e323d0e2b4616fef68a8ddd63'
-string = "AQAxO6R-Kz17MQueNLTtEG_tYoL73k8kyhmysmR8VMwmiHkgHzSVrjiIpVVmrzx_vRtu4YOaawAcgHj1HzsLGZ1LpfFSki2Ic3luyNYsYxOCNFShVVV2gPoMGTUAEkrTUvK-3MFggnrFPMzULru8v3AtTvR-bTDfgvqvqO2lMDaFk_q_zILvG2jEyGQvlJf2BPgBoPTqND8VaaETrn3GxpVdo7q4TmE-XSAEHZbSHK4f5Vfpuv5mhXRX8ZrxN4KVuANqU-jX-HrGT0a_mYOkuAxl4mWb14xEMwnJ7qgHDz21E2kQLL8nXmy4dHi-7PVrzSKpz7k0grwgPTYWx_N391yEQSNu3gA"
+api_id= api_id #5282591
+api_hash= api_hash #'d416fe4e323d0e2b4616fef68a8ddd63'
+string = string #"AQAxO6R-Kz17MQueNLTtEG_tYoL73k8kyhmysmR8VMwmiHkgHzSVrjiIpVVmrzx_vRtu4YOaawAcgHj1HzsLGZ1LpfFSki2Ic3luyNYsYxOCNFShVVV2gPoMGTUAEkrTUvK-3MFggnrFPMzULru8v3AtTvR-bTDfgvqvqO2lMDaFk_q_zILvG2jEyGQvlJf2BPgBoPTqND8VaaETrn3GxpVdo7q4TmE-XSAEHZbSHK4f5Vfpuv5mhXRX8ZrxN4KVuANqU-jX-HrGT0a_mYOkuAxl4mWb14xEMwnJ7qgHDz21E2kQLL8nXmy4dHi-7PVrzSKpz7k0grwgPTYWx_N391yEQSNu3gA"
 #------------------------------------end
 idss = []
-channel = -1001390839405
 
 ub = Client(string, api_id=api_id, api_hash=api_hash, sleep_threshold=60)
 
@@ -16,11 +28,11 @@ ub = Client(string, api_id=api_id, api_hash=api_hash, sleep_threshold=60)
     
 def clean_data():
     print("checking media")
-    for ids in ub.search_messages(chat_id=-1001241414282, filter="photo_video", limit=20):
+    for ids in ub.search_messages(chat_id=group, filter="photo_video", limit=20):
         msg_id = ids.message_id
         idss.append(msg_id)
-        ub.copy_message(chat_id=channel, from_chat_id=-1001241414282, message_id=msg_id)
-        ub.delete_messages(chat_id=-1001241414282, message_ids=msg_id)
+        ub.copy_message(chat_id=channel, from_chat_id=message.chat.id, message_id=msg_id)
+        ub.delete_messages(chat_id=message.chat.id, message_ids=msg_id)
     else:
         if len(idss) == 0:
             print("no photos to delete")
@@ -30,11 +42,11 @@ def clean_data():
             print(f"cleared almost {c} messages")
             idss.clear() 
 
-    for ids in ub.search_messages(chat_id="acd321", filter="document", limit=5):
+    for ids in ub.search_messages(chat_id=group, filter="document", limit=5):
         msg_id = ids.message_id
         idss.append(msg_id)
-        ub.copy_message(chat_id=channel, from_chat_id=-1001241414282, message_id=msg_id)
-        ub.delete_messages(chat_id=-1001241414282, message_ids=msg_id)
+        ub.copy_message(chat_id=channel, from_chat_id=message.chat.id, message_id=msg_id)
+        ub.delete_messages(chat_id=message.chat.id, message_ids=msg_id)
     else:
         if len(idss) == 0:
             print("no photos to delete")
@@ -73,13 +85,13 @@ def channel_delete():
             idss.clear()  
     
 scheduler = BackgroundScheduler()
-scheduler.add_job(clean_data, 'interval' , seconds=30)
+scheduler.add_job(clean_data, 'interval' , seconds=g_time)
 
 
 scheduler.start()   
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(channel_delete, 'interval' , minutes=30)
+scheduler.add_job(channel_delete, 'interval' , minutes=c_time)
 
 
 scheduler.start()  
