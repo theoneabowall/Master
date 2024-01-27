231d070a8ef8d76acecb9b268c83a251d2189457
alanwalker95830 <71916738+alanwalker95830@users.noreply.github.com>
2024-01-27 15:10:29 +0530

    Update ubmedia.py

--- ubmedia.py
+++ ubmedia.py
@@ -1,10 +1,6 @@
-#Dangerprobot
-from pyrogram import Client,filters
-from collections.abc import MutableMapping
+from pyrogram import *
+from pyrogram.types import *
 from apscheduler.schedulers.background import BackgroundScheduler
-import os
-from os import getenv
-from pyrogram.types.messages_and_media import message
 
 
 
@@ -14,15 +10,9 @@ api_hash_pyrogram = getenv("API_HASH")
 string_pyrogram = getenv("SESSION_STRING")
 g_time = int(getenv("GROUP_DELETE_TIME"))
 c_time = int(getenv("CHANNEL_DELETE_TIME"))
-group = int(getenv("GROUP_ID"))
-channel = int(getenv("CHANNEL_ID"))
 
-#---------------------------------+ Data
-api_id= api_id_pyrogram 
-api_hash= api_hash_pyrogram 
-session_string= string_pyrogram 
-group = -1002120547379
-channel = -1002025718840
+
+
 #------------------------------------end
 idss = []
 
@@ -34,12 +24,12 @@ app = Client(name="auto-delete",session_string =string_pyrogram, api_id=api_id_p
 def clean_data():
     print("checking media")
     idss = []
-    for message in app.search_messages(chat_id=group,filter="filters.photo", limit=20):
-        msg_id = message.message.id
+    for message in app.search_messages(chat_id=group, filter=enums.MessagesFilter.PHOTO_VIDEO, limit=3):
+        msg_id = message.id
         idss.append(msg_id)
         app.copy_message(chat_id=channel, from_chat_id=group, message_id=msg_id)
         app.delete_messages(chat_id=group, message_ids=msg_id)
-    try:
+    else:
         if len(idss) == 0:
             print("no photos to delete")
             return
@@ -47,17 +37,15 @@ def clean_data():
             c = len(idss)
             print(f"cleared almost {c} messages")
             idss.clear() 
-    except Exception as e:
-        print(e) 
 
-
+    
 def channel_delete():
     print("trying to delete channel messages")
-    for message in app.search_messages(chat_id=channel, filter="media"):
-        msg_id = message.message.id
+    for message in app.search_messages(chat_id=channel, filter=enums.MessagesFilter.PHOTO_VIDEO):
+        msg_id = message.id
         idss.append(msg_id)
         app.delete_messages(chat_id=channel, message_ids=msg_id)
-    try:
+    else:
         if len(idss) == 0:
             print("no photos to delete")
             return
@@ -65,8 +53,7 @@ def channel_delete():
             c = len(idss)
             print(f"almost {c} files deleted")
             idss.clear() 
-    except Exception as e:
-        print(e)
+
     
     
 scheduler = BackgroundScheduler(timezone="Asia/Kolkata")
